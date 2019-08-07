63945c6adf1bc25e574db5a9f836e9ff9c5371e1
Billiam <billiamthesecond@gmail.com>
2019-08-07 11:02:39 -0500

    Skip build if container exists, fix volume mount

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -ev
 
-docker build . -t thingiverse
+if [[ "$(docker images -q thingiverse 2> /dev/null)" == "" ]]; then
+  docker build . -t thingiverse
+fi
 
 docker run -it --rm \
--v "${pwd}:/usr/src/tv" \
+-v "$(pwd):/usr/src/tv" \
 thingiverse "$@"
